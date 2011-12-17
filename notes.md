3b6ded7557e3b6493ae2a52b85f271c34f24cb52
Craig Andera <candera@wangdera.com>
2011-12-17 09:06:02 -0500

    Further progress.

---
+++ notes.md
@@ -0,0 +1,5 @@
+# Things to do
+
+* Set up redirect. Looks like it's going to have to be to a month page
+  - not sure I have enough control over the URLs to make redirects to
+  individual posts.
--- scratch.clj
+++ scratch.clj
@@ -55,7 +55,7 @@
 
 (use 'clojure.pprint)
 
-(def posts (let [conn (SqlConnection. "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=CommunityServer;Data Source=.")
+(def all-posts (let [conn (SqlConnection. "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=CommunityServer;Data Source=.")
                  _ (.Open conn)]
              (select conn "select * from dbo.cs_Posts")))
 
@@ -63,6 +63,21 @@
                  _ (.Open conn)]
              (select conn "select * from dbo.cs_Threads")))
 
+
+(def posts (group-by #(get % "ParentID") all-posts))
+
+(def toplevel-posts (filter #(= (get % "PostID") (get % "ParentID")) all-posts))
+
+(defn comments
+  "Given the list of all posts, return a seq of the posts that are
+  comments on a particular post."
+  [all-posts post]
+  )
+
+(def posts-with-comments
+  (map #() (filter (fn [p] ((get p "ParentID"))) toplevel-posts)))
+
+(comment (binding [*print-level* 3] (pprint posts)))
 
 (def authors (distinct (map #(get % "PostAuthor") threads)))
 
@@ -73,11 +88,88 @@
   [posts thread]
   (filter #(= (get thread "ThreadID") (get % "ThreadID")) posts))
 
-(->> (threads-by-author "craig-andera")
-     first
-     (posts-for-thread posts)
-     (take 5)
-     pprint
-     )
+(comment (->> (threads-by-author "craig-andera")
+              (take 3)
+              (posts-for-thread posts)
+              (take 5)
+              pprint
+              ))
 
-(pprint (threads-by-author "craig-andera"))
+(def threads-with-posts
+  (map #(assoc % :posts (posts-for-thread posts %)) threads))
+
+;; (binding [*print-length* 10] (pprint (take 1 threads-with-posts)))
+
+;; ((first threads-with-posts) "PostAuthor")
+
+;; (pprint (threads-by-author "craig-andera"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(System.Reflection.Assembly/LoadWithPartialName "System.Xml")
+(import '[System.Xml XmlTextWriter Formatting XmlConvert])
+(import 'System.DateTime)
+
+;; (def writer (XmlTextWriter. "C:\\temp\\test.atom" nil))
+
+;; (.set_Formatting writer Formatting/Indented)
+
+;; (.WriteElementString writer "test" "foo")
+
+;; (.Close writer)
+
+(def atom-ns "http://www.w3.org/2005/Atom")
+
+;; Comments have postlevel = 2 and ParentID != PostID
+
+(defn write-post-entry
+  "Given a thread (with posts) write the entries for it to the XmlWriter."
+  [writer thread]
+  (write-entry writer
+               (thread "ThreadID")      ; id
+               (thread "PostDate")      ; published & updated
+               (thread "ThreadDate")    ; title
+               ))
+
+(defn write-post-entries
+  "Given a seq of threads (with posts), write the entries for them to
+  the XmlWriter."
+  [writer threads]
+  (doall (map (partial write-post-entry writer) threads)))
+
+(defn write-feed
+  "Spit out an Atom feed file with the specified name, given the threads."
+  [path threads]
+  (doto (XmlTextWriter. path nil)
+    (.set_Formatting Formatting/Indented)
+    (.WriteStartElement "feed" atom-ns)
+    (.WriteElementString "id" atom-ns "feed-id")
+    (.WriteElementString "updated" atom-ns (XmlConvert/ToString (DateTime/Now)))
+
+    (.WriteStartElement "title" atom-ns)
+    (.WriteAttributeString "type" "text")
+    (.WriteValue "Blog Title Here")
+    (.WriteEndElement)
+
+    (.WriteStartElement "generator" atom-ns)
+    (.WriteAttributeString "version" "7.00")
+    (.WriteAttributeString "uri" "http://www.blogger.com")
+    (.WriteValue "Blogger")
+    (.WriteEndElement)
+
+    (write-post-entries threads)
+
+    (.WriteStartElement "entry" atom-ns)
+    (.WriteValue "TODO")
+    (.WriteEndElement)
+
+    (.Close)))
+
+(comment (write-feed "C:\\temp\\test3.atom" threads))
+
+(def some-threaded-posts
+  (let [some-threads (->> (threads-by-author "craig-andera")
+                          (take 3))]
+    (map #(assoc % :posts (take 3 (posts-for-thread posts %))) some-threads)))
+
+(pprint some-threaded-posts)
