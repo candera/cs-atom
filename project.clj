567a35824d9b26e98b6d058dfe78b63b428b5df1
Craig Andera <candera@wangdera.com>
2011-12-22 07:40:06 -0500

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cs-atom "0.1.0-SNAPSHOT"
+  :description "Some scripts for getting data out of a CommunityServer
+  SQL database and into an Atom feed."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [enlive "1.0.0"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
