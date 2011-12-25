2f13c6dd131e6b2e074a79dc7a98184a0426cb06
Craig Andera <candera@wangdera.com>
2011-12-25 10:06:33 -0600

    Update scratch code.

--- scratch.clj
+++ scratch.clj
@@ -1,2 +1,17 @@
+(def a-post (nth (content-by-author "craig-andera") 100))
+
+(count (:tidied-body a-post))
+(count (a-post "Body"))
+(count (a-post "FormattedBody"))
+
+(let [nodes (html-resource (StringReader. (a-post "Body")))
+      ;nodes (remove-blank-p-tags nodes)
+      ]
+  (count (apply str
+          (emit* (if-let [body (select nodes [:html :body content])]
+                   body
+                   nodes)))))
 
 
+(pprint
+ (select (html-resource (StringReader. (a-post "Body"))) [:html :body :> content]))
